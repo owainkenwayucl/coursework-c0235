04cae888d2c34148eb455804129919d47a4be8b3
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-09 15:53:26 +0100

    Remove unnecessary varables

--- lecture3a_dataengexample/key_management/terraform/varibles.tf
+++ lecture3a_dataengexample/key_management/terraform/varibles.tf
@@ -1,26 +1,10 @@
-variable img_display_name {
-  type = string
-  default = "almalinux-9.4-20240805"
-}
-
 variable namespace {
   type = string
 #  default = "ds4eng-ns"
 }
 
-variable network_name {
-  type = string
-  default = "ds4eng-ns/ds4eng"
-}
-
 variable username {
   type = string
 #  default = ""
-}
-
-
-variable vm_count {
-  type    = number
-  default = 1
 }
 
