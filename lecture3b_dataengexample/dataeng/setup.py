3eeda4f871388b4eb48d27fa7575f851046d3907
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-23 11:12:43 +0100

    Add in pip stuff

---
+++ lecture3b_dataengexample/dataeng/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dataeng',
+      version='1.1',
+      description='Python code for the Data Engineering Course Example',
+      author='Dr Owain Kenway',
+      author_email='o.kenway@ucl.ac.uk',
+      url='TBC',
+      packages=['dataeng'],
+      install_requires=['bs4','requests', 'mr4mp', 'lxml'],
+      entry_points={'console_scripts': ['build-index = dataeng.gather:parse_index_entry',
+                                        'analyse = dataeng.analysis:analysis_entry',
+                                        'combine = dataeng.combine:combine_entry'
+                                        ]}
+     )
